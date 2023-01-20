e78aedd6a5249f02af31534ccbe15810c1d2f253
Lucas <lucas.fuentes@student.vinci.be>
2023-01-20 10:44:44 +0100

    test(validation.test): add first test whith length

--- src/services/validations.js
+++ src/services/validations.js
@@ -1,5 +1,11 @@
 // TODO: Add here the function to validate
+function isValidGamerTag(gamerTag) {
+    if(gamerTag.length>=8) return true;
+
+    return false;
+}
+
 
 const isEmpty = (label) => !label || label.length === 0;
 
-exports.isEmpty = isEmpty;
+module.exports = { isValidGamerTag, isEmpty};
